d301e25150c45528149709884c37e9feade283dd
matsueushi <matsueushi@gmail.com>
2020-11-04 15:22:42 +0000

    4.1.11

--- src/data.jl
+++ src/data.jl
@@ -13,8 +13,8 @@ end
 function regression_data1()
     path = "../data/house-prices-advanced-regression-techniques/train.csv"
     data = CSV.read(path, DataFrame)
-    X = Array(data[data[:MSSubClass] .== 60, [:GrLivArea]])
-    Y = data[data[:MSSubClass] .== 60, :SalePrice]
+    X = Array(data[data.MSSubClass .== 60, [:GrLivArea]])
+    Y = data[data.MSSubClass .== 60, :SalePrice]
     x_label = "居住面積x[平方フィート]"
     y_label = "物件価格y[ドル]"
     return RegressionData(X, Y, x_label, y_label)
@@ -25,8 +25,8 @@ end
 function regression_data2()
     path = "../data/house-prices-advanced-regression-techniques/train.csv"
     data = CSV.read(path, DataFrame)
-    X = Array(data[data[:MSSubClass] .== 60, [:GrLivArea, :GarageArea, :PoolArea, :BedroomAbvGr, :TotRmsAbvGrd]])
-    Y = data[data[:MSSubClass] .== 60, :SalePrice]
+    X = Array(data[data.MSSubClass .== 60, [:GrLivArea, :GarageArea, :PoolArea, :BedroomAbvGr, :TotRmsAbvGrd]])
+    Y = data[data.MSSubClass .== 60, :SalePrice]
     x_label = ""
     y_label = "物件価格y[ドル]"
     return RegressionData(X, Y, x_label, y_label)
@@ -37,8 +37,8 @@ end
 function regression_data3()
     path = "../data/house-prices-advanced-regression-techniques/train.csv"
     data = CSV.read(path, DataFrame)
-    X = Array(data[data[:MSSubClass] .== 60, [:GrLivArea]])
-    Y = data[data[:MSSubClass] .== 60, :SalePrice]
+    X = Array(data[data.MSSubClass .== 60, [:GrLivArea]])
+    Y = data[data.MSSubClass .== 60, :SalePrice]
     Y[Y .> 700000] -= 700000  # 外れ値
     x_label = "居住面積x[平方フィート]"
     y_label = "物件価格y[ドル]"
